04c90b5160d561312052cfb91dfd886eb35d144c
VasaviYakkali <85737909+VasaviYakkali@users.noreply.github.com>
2021-07-29 14:50:35 +0530

    Update NewSaleService.java

--- new-sale-api/src/main/java/com/newsaleapi/service/NewSaleService.java
+++ new-sale-api/src/main/java/com/newsaleapi/service/NewSaleService.java
@@ -1,12 +1,15 @@
 package com.newsaleapi.service;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.newsaleapi.vo.BarcodeVo;
 import com.newsaleapi.vo.CustomerDetails;
 import com.newsaleapi.vo.DeliverySlipVo;
 import com.newsaleapi.vo.NewSaleVo;
+import com.newsaleapi.vo.TaxVo;
 
 
 @Component
@@ -19,5 +22,7 @@ public interface NewSaleService {
 	ResponseEntity<?> getBarcodeDetails(String barCode);
 
 	ResponseEntity<?> saveDeliverySlip(DeliverySlipVo vo);
+	
+	List<TaxVo> getNewSaleWithTax() throws JsonMappingException, JsonProcessingException;
 
 }
